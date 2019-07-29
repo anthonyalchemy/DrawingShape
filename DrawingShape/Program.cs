1be43ddc30dcbff28d1596ce69307622cc326dfb
anthonyalchemy <anthony@alchemytechs.com>
2019-07-29 10:11:17 +0100

    Add project files.

---
+++ DrawingShape/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DrawingShape 
+    //DrawingSpace project 
+{
+    class Program
+        //class is a container where we store code
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /___|");
+
+            //Runs first, then second and so on  
+
+            Console.ReadLine();
+            //Keeps console window open (ReadLine)
+        }
+    }
+}
